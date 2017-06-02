201b84fadc51ad2db4e4e5c8a9cbb060dae5fc4c
rhslee2000 <rhslee2000@yahoo.com.au>
2017-06-03 09:54:16 +1000

    init commit

---
+++ src/main/java/com/madman/blog/dockermaven/DockerMavenApplication.java
@@ -0,0 +1,12 @@
+package com.madman.blog.dockermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerMavenApplication.class, args);
+	}
+}
